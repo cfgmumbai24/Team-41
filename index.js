a9beef0a0f0cca7aa55fda2185a0e69d13be01e6
Shiv Anand Awasthi <shivaaya.2003@gmail.com>
2024-06-15 23:32:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import makeconnection from "./connection/connection.js";
+import cors from 'cors';
+import dotenv from 'dotenv';
+import routes2 from "./routes/adminroutes.js";
+import router from "./routes/form.js";
+const port = 8000;
+const app = express();
+dotenv.config();
+makeconnection(process.env.DB_username,process.env.DB_password);
+app.use(cors());
+app.get('/',(req,res)=>{
+    res.send("Hello world");
+});
+app.use(express.json({extended:false}));
+app.use('/api/users',routes2);
+app.use('/api/forms',router);
+app.listen(port,()=>{
+    console.log("Port is running on",port);
+});
