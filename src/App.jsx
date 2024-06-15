4edc63d31371f49bdb0baaa3cbb2fe1000881302
Amlan Sarangi <117941624+amlansarangi23@users.noreply.github.com>
2024-06-15 22:46:19 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import VolunteerPage from './volunteerpage';
-import BuyPage from './components/BuyPage';
-import BeneficiaryProfile from './components/BeneficiaryProfile';
-import VendorApp from './vendorpage';
+import React from "react";
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import VolunteerPage from "./volunteerpage";
+import BuyPage from "./components/BuyPage";
+import BeneficiaryProfile from "./components/BeneficiaryProfile";
+import VendorApp from "./vendorpage";
+import UserAdmin from "./useradmin";
+import Dashboard from "./pages/admin/dashboard";
+const Home = lazy(() => import("./pages/home"));
+const Search = lazy(() => import("./pages/search"));
+const Cart = lazy(() => import("./pages/cart"));
+const Shipping = lazy(() => import("./pages/shipping"));
+const Login = lazy(() => import("./pages/login"));
+const Orders = lazy(() => import("./pages/orders"));
+import Loader from "./components/Loader";
+import Header from "./components/header";
+import "./styles/app.scss";
 
-
+//admin routes
+import Products from "./pages/admin/products";
+import Customers from "./pages/admin/customers";
+import Transaction from "./pages/admin/transaction";
+import Barcharts from "./pages/admin/charts/barcharts";
+import Piecharts from "./pages/admin/charts/piecharts";
+import Linecharts from "./pages/admin/charts/linecharts";
+import Coupon from "./pages/admin/apps/coupon";
+import Stopwatch from "./pages/admin/apps/stopwatch";
+import Toss from "./pages/admin/apps/toss";
+import NewProduct from "./pages/admin/management/newproduct";
+import ProductManagement from "./pages/admin/management/productmanagement";
+import TransactionManagement from "./pages/admin/management/transactionmanagement";
 
 const App = () => {
   return (
@@ -14,11 +38,44 @@ const App = () => {
         <Route path="/volunteer" element={<VolunteerPage />} />
         <Route path="/buypage" element={<BuyPage />} />
         <Route path="/vendor" element={<VendorApp />} />
+        <Route path="/useradmin" element={<UserAdmin />} />
+        <Route path="/admin/dashboard" element={<Dashboard />} />
         {/* Other routes */}
+        <Route path="/beneficiary/:id" element={<BeneficiaryProfile />} />
+        <Route path="/buyerhome" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/cart" element={<Cart />} />
+
+        <Route path="/login" element={<Login />} />
+
+        {/* logged in user routes */}
+
+        <Route path="/shipping" element={<Shipping />} />
+
+        <Route path="/orders" element={<Orders />} />
+
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/product" element={<Products />} />
+        <Route path="/admin/customer" element={<Customers />} />
+        <Route path="/admin/transaction" element={<Transaction />} />
+        {/* Charts */}
+        <Route path="/admin/chart/bar" element={<Barcharts />} />
+        <Route path="/admin/chart/pie" element={<Piecharts />} />
+        <Route path="/admin/chart/line" element={<Linecharts />} />
+        {/* Apps */}
+        <Route path="/admin/app/coupon" element={<Coupon />} />
+        <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
+        <Route path="/admin/app/toss" element={<Toss />} />
+
+        {/* Management */}
+        <Route path="/admin/product/new" element={<NewProduct />} />
+
+        <Route path="/admin/product/:id" element={<ProductManagement />} />
+
         <Route
-            path="/beneficiary/:id"
-            element={<BeneficiaryProfile />}
-          />
+          path="/admin/transaction/:id"
+          element={<TransactionManagement />}
+        />
       </Routes>
     </Router>
   );
---
+++ src/components/header.tsx
@@ -0,0 +1,63 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+
+import "../styles/app.scss";
+
+const user = { _id: "njsdhf", role: "admin" };
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const logoutHandler = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <nav className="header">
+      <Link to={"/"}>
+        <img alt="Logo" className="logo" /> {/* Add the logo */}
+      </Link>
+      <div className="nav-links">
+        <Link onClick={() => setIsOpen(false)} to={"/"}>
+          HOME
+        </Link>
+        <Link onClick={() => setIsOpen(false)} to={"/search"}>
+          <FaSearch />
+        </Link>
+        <Link onClick={() => setIsOpen(false)} to={"/cart"}>
+          <FaShoppingBag />
+        </Link>
+        {user?._id ? (
+          <>
+            <button onClick={() => setIsOpen((prev) => !prev)}>
+              <FaUser />
+            </button>
+            <dialog open={isOpen}>
+              <div>
+                {user.role === "admin" && (
+                  <Link onClick={() => setIsOpen(false)} to="/admin/dashboard">
+                    Admin
+                  </Link>
+                )}
+                <Link onClick={() => setIsOpen(false)} to="/orders">
+                  Orders
+                </Link>
+                <button onClick={logoutHandler}>
+                  <FaSignOutAlt />
+                </button>
+              </div>
+            </dialog>
+          </>
+        ) : (
+          <Link onClick={() => setIsOpen(false)} to={"/login"}>
+            <FaSignInAlt />
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
+
